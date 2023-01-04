60209933fd4f851a3ee3d0d755fcc7fc124c2c9c
Benjamin Krichman <benkrichman@gmail.com>
2023-01-03 18:39:06 -0600

    updated version in setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md','r') as ldf:
 
 st.setup(
     name='datascale',
-    version='1.1.0',
+    version='1.1.1',
     license='MIT',
     url='https://github.com/benkrichman/datascale',
     description='Functions for automatic scaling of matplotlib plot axes/resolution to data',
